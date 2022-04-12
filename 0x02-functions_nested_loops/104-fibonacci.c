e4c0309ee16f275bedaa23c27bfe6d2d74bd8434
Shadow_Hubb <ntamkaz@gmail.com>
2022-04-12 14:02:04 -0700

    Task 16 commit

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.
+ *The numbers should be separated by comma, followed by a space ,
+ *You are allowed to use the standard library
+ *You are not allowed to use any other library (You can’t use GMP etc…)
+ *You are not allowed to use long long, malloc, pointers, arrays/tables, or structures
+ *You are not allowed to hard code any Fibonacci number (except for 1 and 2)
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+	unsigned long int bef1;
+	unsigned long int bef2;
+	unsigned long int aft1;
+	unsigned long int aft2;
+
+	printf("%lu", bef);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
+	}
+	printf("\n");
+	return (0);
+}
